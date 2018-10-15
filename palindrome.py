3a6416dbfd92f53a76c1e983d74d9f5545828310
Mustafa Çoban <32637193+b21627155@users.noreply.github.com>
2018-10-15 21:21:41 +0300

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,3 +1,8 @@
+#Question:
+"""Write a function that determines if the given input string is a palindrome or is not
+A palindrome is a sequence of characters which reads the same backward as forward"""
+
+#solition:
 def palindrome():
     string=input("enter string")
     string1=string[::-1]
